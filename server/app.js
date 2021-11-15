0443f69556b98872735e6e2e19bdede41f2d9c47
gustavoclay <gustavoclay@gmail.com>
2021-11-15 03:41:51 -0300

    v2 node server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+app.get('/', (req, res) => res.send('<h1>Hello World in K8s!</h1>'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
